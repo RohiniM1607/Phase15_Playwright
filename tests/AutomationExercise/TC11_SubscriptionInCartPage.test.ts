08e14be4ee8745c194fededf4c99d8a37e24c4bd
RohiniM1607 <mrohini7558@gmail.com>
2026-06-28 17:35:04 +0530

    Automation Exercise - TestCase-11

---
+++ tests/AutomationExercise/TC11_SubscriptionInCartPage.test.ts
@@ -0,0 +1,34 @@
+import {test, expect} from '@playwright/test';
+
+test('Subscription cart page', async({page})=>{
+    await page.route('**/*', route => {
+        const url = route.request().url();
+
+        if (
+            url.includes('googleads') ||
+            url.includes('doubleclick') ||
+            url.includes('googlesyndication') ||
+            url.includes('adservice')
+        ) {
+            route.abort();
+        } else {
+            route.continue();
+        }
+    });
+    await page.goto("http://automationexercise.com");
+    await expect(page.locator('.carousel-inner').first()).toBeVisible();
+    console.log("Home page - Verified");
+
+    await page.getByRole('link', {name: " Cart"}).click();
+    await expect(page).toHaveURL("https://automationexercise.com/view_cart");
+    console.log("Cart page - Verified");
+
+    await page.locator("footer").scrollIntoViewIfNeeded();
+    await expect(page.locator(".single-widget h2")).toHaveText("Subscription");
+    await page.locator("#susbscribe_email").fill("demo.1@gmail.com");
+    await page.locator('#subscribe').click();
+    console.log("Mail send");
+    await expect(page.locator(".alert-success")).toContainText("You have been successfully subscribed!");
+    console.log("Successfully subscribed in home page");
+
+})
