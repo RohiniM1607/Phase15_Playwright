e3409c2ec2a7ee44a6b4707c2272cc8ec0077408
RohiniM1607 <mrohini7558@gmail.com>
2026-06-28 13:28:35 +0530

    Config modified

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: `./env/.env.${envName}` });
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 500000,
+  timeout: 700000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
