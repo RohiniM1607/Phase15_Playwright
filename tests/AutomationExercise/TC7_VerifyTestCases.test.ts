d3210f91c816c8274a6f7c980652c1b13daacf96
RohiniM1607 <mrohini7558@gmail.com>
2026-06-28 13:27:50 +0530

    Automation Exercise - TestCase-7

---
+++ tests/AutomationExercise/TC7_VerifyTestCases.test.ts
@@ -0,0 +1,12 @@
+import {test, expect} from '@playwright/test';
+
+test('Verify Page', async({page}) =>{
+    await page.goto("http://automationexercise.com");
+    await expect(page.locator('.carousel-inner').first()).toBeVisible();
+    console.log("Home page - Verified");
+
+    await page.locator("//a[text()=' Test Cases']").click();
+    console.log("Test cases clicked");
+    await expect(page).toHaveURL("https://automationexercise.com/test_cases");
+    console.log("Test case page - Verified");
+})
