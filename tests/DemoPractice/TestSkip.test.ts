1e36615a7df14511be9faeb87ac3bc533ad40e5a
RohiniM1607 <mrohini7558@gmail.com>
2026-06-29 16:58:16 +0530

    Updated

--- tests/DemoPractice/TestSkip.test.ts
+++ tests/DemoPractice/TestSkip.test.ts
@@ -7,9 +7,8 @@ test('Login test 1',async({page})=>{
     await page.click('button[onclick="logIn()"]');
     const welcometxt = page.locator("#nameofuser").textContent();
     
-    await expect.soft(welcometxt).tohavetext('Welcome RohiniM');
 })
-test.only("Login Test 2",async({page})=>{
+test("Login Test 2",async({page})=>{
      await page.goto("https://www.demoblaze.com/");
     await page.click('#login2')
     await page.fill('#loginusername',"RohiniM");
